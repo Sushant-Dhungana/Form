b59cc2384afe89cbf2975c4802bc63ebcd4b9ee9
Sushant-Dhungana <sushantdhungana29@gmail.com>
2022-06-24 22:17:45 +0545

    address 1 comitted

--- src/components/AddressOne.jsx
+++ src/components/AddressOne.jsx
@@ -1,117 +1,139 @@
-// import React from 'react'
-// import { Button, Form, Input } from 'antd';
+import React,{useState} from 'react'
+import { Button, Dropdown, Form, Input } from 'antd';
+import data from '../data';
 
 
-// const AddressOne = () => {
-//   return (
-//     <div>
-//         <h2>Address 1</h2>
-//     <Form
-//       name="basic"
-//       labelCol={{
-//         span: 8,
-//       }}
-//       wrapperCol={{
-//         span: 16,
-//       }}
-//       initialValues={{
-//         remember: true,
-//       }}
+const AddressOne = ({initialdata, setValues}) => {
+
+const [fields, setFields] = useState({
+  id:1,
+  
+})
+
+  return (
+    <div>
+        <h2>Address 1</h2>
+  
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
     
-//       autoComplete="off" 
-//     >
-//       <Form.Item
-//         label="Office Type*"
-//         name="officetype"
-//         rules={[
-//           {
-//             required: true,
-//             message: 'Please enter your office type',
-//           },
-//         ]}
-//       >
-//         <Input type="text" placeholder='e.g: HQ, branch, etc'  
-//            />
-//       </Form.Item>
-//       <Form.Item
-//         label="Country"
-//         name="country"
-//         rules={[
-//           {
-//             required:true,
-//             message: 'enter a valid website',
-//           },
-//         ]}
-//       >
-//         <select name="country" id="country">
-//   <option value="usa">USA</option>
-//   <option value="nepal">Nepal</option>
-//   </select>
+      autoComplete="off" 
+  
+    >
+      <Form.Item
+        label="Office Type*"
+        name="officetype"
+        rules={[
+          {
+            required: true,
+            message: 'Please enter your office type',
+          },
+        ]}
+      >
+        <Input type="text" placeholder='e.g: HQ, branch, etc' value={initialdata.officetype} 
+        onChange={(e)=>{
+          setValues({...initialdata, officetype:e.target.value})
+        }}  
+           />
+      </Form.Item>
+      <Form.Item
+        label="Country"
+        name="country"
+      >
+  <select name="country" id="country" onChange={(e)=>setValues({...initialdata, country:e.target.value})} >
+  <option value="usa">USA</option>
+  <option value="nepal">Nepal</option>
+  </select>
            
-//       </Form.Item>
-//       <Form.Item
-//         label="Address 1"
-//         name="Address 1"
-//         rules={[
+      </Form.Item>
+      <Form.Item
+        label="Address1"
+        name="Address1"
+        rules={[
           
-//         ]}
-//       >
-//         <Input type="text" placeholder='Enter Address 1 '/>
-//       </Form.Item>
-//       <Form.Item
-//         label="Address 2"
-//         name="Address 2"
-//         rules={[
+        ]}
+      >
+        <Input type="text" placeholder='Enter Address 1 'value={initialdata.Address1} 
+        onChange={(e)=>{
+          setValues({...initialdata, Address1:e.target.value})
+        }} />
+      </Form.Item>
+      <Form.Item
+        label="Address2"
+        name="Address2"
+        rules={[
           
-//         ]}
-//       >
-//         <Input type="text" placeholder='Enter Address 2 '  
-//           />
-//       </Form.Item> 
-//       <Form.Item
-//         label="zip/postal code"
-//         name="Zip/Postal Code"
-//         rules={[
+        ]}
+      >
+        <Input type="text" placeholder='Enter Address 2' value={initialdata.Address2} 
+        onChange={(e)=>{
+          setValues({...initialdata, Address2:e.target.value})
+        }}     
+          />
+      </Form.Item> 
+      <Form.Item
+        label="zip/postal code"
+        name="zip"
+        rules={[
           
-//         ]}
-//       >
-//         <Input type="text" placeholder='Enter zip/postal code'  
-//           />
-//       </Form.Item> 
-//       <Form.Item
-//         label="city"
-//         name="city"
-//         rules={[
+        ]}
+      >
+        <Input type="text" placeholder='Enter zip/postal code' value={initialdata.zip} 
+        onChange={(e)=>{
+          setValues({...initialdata, zip:e.target.value})
+        }}  
+          />
+      </Form.Item> 
+      <Form.Item
+        label="city"
+        name="city"
+        rules={[
           
-//         ]}
-//       >
-//         <Input type="text" placeholder='Enter city'  
-//           />
-//       </Form.Item> 
-//       <Form.Item
-//         label="State"
-//         name="State"
-//         rules={[
+        ]}
+      >
+        <Input type="text" placeholder='Enter city'value={initialdata.city} 
+        onChange={(e)=>{
+          setValues({...initialdata, city:e.target.value})
+        }}  
+          />
+      </Form.Item> 
+      <Form.Item
+        label="State"
+        name="State"
+        rules={[
           
-//         ]}
-//       >
-//         <Input type="text" placeholder='Enter State '  
-//           />
-//       </Form.Item> 
+        ]}
+      >
+        <Input type="text" placeholder='Enter State ' value={initialdata.state} 
+        onChange={(e)=>{
+          setValues({...initialdata, state:e.target.value})
+        }} 
+          />
+      </Form.Item> 
+      </Form>
+
 
     
 
-//       <Form.Item
-//         wrapperCol={{
-//           offset: 8,
-//           span: 16,
-//         }}
-//       >
-//       </Form.Item>
-//     </Form>
-//     </div>
-//   )
-// }
+     
+       <Button >+</Button>
+ 
+      
+       <Button>-</Button>
+     
+    
+    </div>
+  )
+}
 
-// export default AddressOne
+export default AddressOne
 
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,129 +1,150 @@
-import { useState, useEffect, useNavigate } from 'react';
-import { Button, Form, Input } from 'antd';
+import { useState, useEffect, useNavigate } from "react";
+import { Button, Form, Input } from "antd";
+import AddressOne from "./AddressOne";
 // import data from '../data'
 
 const Users = () => {
-    const [initialValues, setInitialValues] = useState({
+  const [initialValues, setInitialValues] = useState({
     companyname: "",
     website: "",
     email: "",
     phone: "",
-   
+    officetype: "",
+    country: "",
+    Address1: "",
+    Address2: "",
+    zip: "",
+    city: "",
+    state: "",
   });
   const [formValues, setFormValues] = useState([]);
 
-  const handleSubmit = () => {
-
-    setFormValues((prevFormValues) => [...prevFormValues, initialValues]);
-  }
-    useEffect(() => {
-
-     localStorage.setItem("formValues", JSON.stringify(formValues));
-     
+  useEffect(() => {
+    localStorage.setItem("formValues", JSON.stringify(formValues));
   }, [formValues]);
   const onFinish = (values) => {
-    console.log('Success:', values);
+    setFormValues((prevFormValues) => [...prevFormValues, initialValues]);
+    console.log("Success:", values);
   };
 
   const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
 
   return (
     <div>
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off" className='form'
-    >
-      <Form.Item
-        label="Company Name*"
-        name="companyname"
-        rules={[
-          {
-            required: true,
-            message: 'Please enter your company name',
-          },
-        ]}
-      >
-        <Input type="text" placeholder='Enter Company Name'  value={initialValues.companyname}
-          onChange={(e) =>
-             setInitialValues({ ...initialValues, companyname: e.target.value })
-           } />
-      </Form.Item>
-      <Form.Item
-        label="Website"
-        name="website"
-        rules={[
-          {
-            required:true,
-            message: 'enter a valid website',
-          },
-        ]}
-      >
-        <Input type="url" placeholder='Enter website ' value={initialValues.website}
-             onChange={(e) =>
-               setInitialValues({ ...initialValues, website: e.target.value })
-            }/>
-      </Form.Item>
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            message: 'enter a email',
-          },
-        ]}
-      >
-        <Input type="email" placeholder='Enter email ' onChange={(e) =>
-             setInitialValues({ ...initialValues, email: e.target.value })
-         }/>
-      </Form.Item>
-      <Form.Item
-        label="Phone"
-        name="phone"
-        rules={[
-          {
-            required:true,
-            message: 'enter 10 digit phone number',
-          },{
-            min:10,max:10
-          }
-        ]}
-      >
-        <Input type="tel" placeholder='Enter phone '  value={initialValues.phone}
-          onChange={(e) =>
-             setInitialValues({ ...initialValues, phone: e.target.value })
-           }/>
-      </Form.Item> 
-
-      <div>
-            <form>
-              
-            </form>
-      </div>
-
-      <Form.Item
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
         wrapperCol={{
-          offset: 8,
           span: 16,
         }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        className="form"
       >
-        <Button type="primary" htmlType="submit"onClick={handleSubmit}>
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Company Name*"
+          name="companyname"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your company name",
+            },
+          ]}
+        >
+          <Input
+            type="text"
+            placeholder="Enter Company Name"
+            value={initialValues.companyname}
+            onChange={(e) =>
+              setInitialValues({
+                ...initialValues,
+                companyname: e.target.value,
+              })
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          label="Website"
+          name="website"
+          rules={[
+            {
+              required: true,
+              message: "enter a valid website",
+            },
+          ]}
+        >
+          <Input
+            type="url"
+            placeholder="Enter website "
+            value={initialValues.website}
+            onChange={(e) =>
+              setInitialValues({ ...initialValues, website: e.target.value })
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              message: "enter a email",
+            },
+          ]}
+        >
+          <Input
+            type="email"
+            placeholder="Enter email "
+            onChange={(e) =>
+              setInitialValues({ ...initialValues, email: e.target.value })
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          label="Phone"
+          name="phone"
+          rules={[
+            {
+              required: true,
+              message: "enter 10 digit phone number",
+            },
+            {
+              min: 10,
+              max: 10,
+            },
+          ]}
+        >
+          <Input
+            type="tel"
+            placeholder="Enter phone "
+            value={initialValues.phone}
+            onChange={(e) =>
+              setInitialValues({ ...initialValues, phone: e.target.value })
+            }
+          />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <div>
+        <AddressOne initialdata={initialValues} setValues={setInitialValues}/>
+      </div>
     </div>
   );
 };
